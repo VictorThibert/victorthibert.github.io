f5336ed6d0c621549d3e4aeb2112fed4c613aa3a
Victor Thibert <victor.thibert@gmail.com>
2016-09-22 22:42:45 -0400

    more modal work

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,25 +1,49 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var tfsaModal = document.getElementById('tfsaModal');
 
 // Get the button that opens the modal
 var tfsaClick = document.getElementById("tfsaguideID");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+
 
 // When the user clicks on the button, open the modal 
 tfsaClick.onclick = function() {
-    modal.style.display = "block";
+    tfsaModal.style.display = "block";
+}
+
+
+
+var radialModal = document.getElementById('radialModal');
+
+// Get the button that opens the modal
+var radialClick = document.getElementById("radialID");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+// When the user clicks on the button, open the modal 
+radialClick.onclick = function() {
+    radialModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+
+
+span[0].onclick = function() {
+    radialModal.style.display = "none";
+    tfsaModal.style.display = "none";
 }
+span[1].onclick = function() {
+    radialModal.style.display = "none";
+    tfsaModal.style.display = "none";
+}
+
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == tfsaModal || event.target == radialModal) {
+        tfsaModal.style.display = "none"
+        radialModal.style.display = "none"
     }
 }
