b569b4dc56de5e58acdf8a7856be0526c310864d
Victor Thibert <victor.thibert@gmail.com>
2016-10-01 19:52:00 -0400

    modal work

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -5,14 +5,12 @@ var tfsaModal = document.getElementById('tfsaModal');
 var tfsaClick = document.getElementById("tfsaguideID");
 tfsaClick.onclick = function() {
     tfsaModal.style.display = "block";
-    // tooltips[1].style.visibility = "hidden";
 }
 
 var radialModal = document.getElementById('radialModal');
 var radialClick = document.getElementById("radialID");
 radialClick.onclick = function() {
     radialModal.style.display = "block";
-    // tooltips[2].style.visibility = "hidden";
 }
 
 // When the user clicks anywhere outside of the modal, close it
