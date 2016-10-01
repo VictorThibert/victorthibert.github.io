437de2eea0cc991822b84703b5f604b4487918f3
Victor Thibert <victor.thibert@gmail.com>
2016-10-01 17:22:16 -0400

    cleaned up index.html style

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -4,14 +4,10 @@ var tfsaModal = document.getElementById('tfsaModal');
 // Get the button that opens the modal
 var tfsaClick = document.getElementById("tfsaguideID");
 
-
-
 // When the user clicks on the button, open the modal 
 tfsaClick.onclick = function() {
     tfsaModal.style.display = "block";
 }
-
-
 
 var radialModal = document.getElementById('radialModal');
 
@@ -27,8 +23,6 @@ radialClick.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-
-
 span[0].onclick = function() {
     radialModal.style.display = "none";
     tfsaModal.style.display = "none";
@@ -37,8 +31,6 @@ span[1].onclick = function() {
     radialModal.style.display = "none";
     tfsaModal.style.display = "none";
 }
-
-
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
