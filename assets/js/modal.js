4bcf3fab0aa40c6461879b798511b57fe7bd2a3b
Victor Thibert <victor.thibert@gmail.com>
2016-10-02 00:52:37 -0400

    models linked to all project pics

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,6 +1,11 @@
 var tooltips = document.getElementsByClassName("tooltiptext");
 
-// Get the modal
+var cherrypickerModal = document.getElementById('cherrypickerModal');
+var cherrypickerClick = document.getElementById("cherrypickerID");
+cherrypickerClick.onclick = function() {
+    cherrypickerModal.style.display = "block";
+}
+
 var tfsaModal = document.getElementById('tfsaModal');
 var tfsaClick = document.getElementById("tfsaguideID");
 tfsaClick.onclick = function() {
@@ -13,10 +18,32 @@ radialClick.onclick = function() {
     radialModal.style.display = "block";
 }
 
+var friendmapModal = document.getElementById('friendmapModal');
+var friendmapClick = document.getElementById("friendmapID");
+friendmapClick.onclick = function() {
+    friendmapModal.style.display = "block";
+}
+
+var compressionModal = document.getElementById('compressionModal');
+var compressionClick = document.getElementById("compressionID");
+compressionClick.onclick = function() {
+    compressionModal.style.display = "block";
+}
+
+var districtModal = document.getElementById('districtModal');
+var districtClick = document.getElementById("districtID");
+districtClick.onclick = function() {
+    districtModal.style.display = "block";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == tfsaModal || event.target == radialModal) {
+    if (event.target == tfsaModal || event.target == radialModal || event.target == cherrypickerModal || event.target == friendmapModal || event.target == compressionModal || event.target == districtModal) {
         tfsaModal.style.display = "none";
         radialModal.style.display = "none";
+        cherrypickerModal.style.display = "none";
+        friendmapModal.style.display = "none";
+        compressionModal.style.display = "none";
+        districtModal.style.display = "none";
     }
 }
